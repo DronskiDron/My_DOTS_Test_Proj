1432dfb201250537ce30070909dca9407a44d543
DronskiDron <andronaf93@gmail.com>
2024-12-12 15:14:35 +0300

    Enableable components were added;

--- Assets/My_DOTS_Test_Proj/Scripts/System/PlayerShootingSystem.cs
+++ Assets/My_DOTS_Test_Proj/Scripts/System/PlayerShootingSystem.cs
@@ -15,6 +15,17 @@ namespace My_DOTS_Test_Proj.Scripts.Components.System
 
         protected override void OnUpdate()
         {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Entity playerEntity = SystemAPI.GetSingletonEntity<Player>();
+                EntityManager.SetComponentEnabled<Stunned>(playerEntity, true);
+            }
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                Entity playerEntity = SystemAPI.GetSingletonEntity<Player>();
+                EntityManager.SetComponentEnabled<Stunned>(playerEntity, false);
+            }
+
             if (!Input.GetKeyDown(KeyCode.Space))
             {
                 return;
@@ -24,7 +35,8 @@ namespace My_DOTS_Test_Proj.Scripts.Components.System
 
             EntityCommandBuffer entityCommandBuffer = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
 
-            foreach (RefRO<LocalTransform> localTransform in SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Player>())
+            foreach (RefRO<LocalTransform> localTransform in SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Player>()
+            .WithDisabled<Stunned>())
             {
                 Entity spawnedEntity = entityCommandBuffer.Instantiate(spawnCubesConfig.CubePrefabEntity);
                 entityCommandBuffer.SetComponent(spawnedEntity, new LocalTransform
@@ -37,5 +49,27 @@ namespace My_DOTS_Test_Proj.Scripts.Components.System
 
             entityCommandBuffer.Playback(EntityManager);
         }
+
+
+        // protected override void OnUpdate()
+        // {
+        //     if (!Input.GetKeyDown(KeyCode.Space))
+        //     {
+        //         return;
+        //     }
+
+        //     SpawnCubesConfig spawnCubesConfig = SystemAPI.GetSingleton<SpawnCubesConfig>();
+
+        //     foreach (RefRO<LocalTransform> localTransform in SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Player>())
+        //     {
+        //         Entity spawnedEntity = EntityManager.Instantiate(spawnCubesConfig.CubePrefabEntity);
+        //         SystemAPI.SetComponent(spawnedEntity, new LocalTransform
+        //         {
+        //             Position = localTransform.ValueRO.Position,
+        //             Rotation = quaternion.identity,
+        //             Scale = 1f
+        //         });
+        //     }
+        // }
     }
 }
